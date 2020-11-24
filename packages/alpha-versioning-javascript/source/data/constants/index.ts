7c8f740cfe76dbc69679b065e66a47f09647eff4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-24 11:21:58 +0200

    alpha-versioning-javascript > dev: data, functions

---
+++ packages/alpha-versioning-javascript/source/data/constants/index.ts
@@ -0,0 +1,16 @@
+// #region module
+const segmentKindMap = {
+    end: 'ender',
+    launch: 'launcher',
+    revision: 'revisioner',
+    development: 'developmenter',
+};
+// #endregion module
+
+
+
+// #region exports
+export {
+    segmentKindMap,
+};
+// #endregion exports
--- packages/alpha-versioning-javascript/source/functions/index.ts
+++ packages/alpha-versioning-javascript/source/functions/index.ts
@@ -1,6 +1,10 @@
 // #region imports
     // #region external
     import {
+        segmentKindMap,
+    } from '../data/constants';
+
+    import {
         AlphaSegments,
         AlphaSegmentKind,
         AlphaUpdateType,
@@ -11,22 +15,108 @@
 
 
 // #region module
+const splitAlphaSegments = (
+    value: string,
+) => {
+    const split = value.split('-');
+    const base = split[0];
+    const developmenterData = split[1];
+
+    if (!base || !developmenterData) {
+        return;
+    }
+
+    const baseSplit = base.split('.');
+    const enderData = baseSplit[0];
+    const launcherData = baseSplit[1];
+    const revisionerData = baseSplit[2];
+
+    if (
+        !enderData
+        || !launcherData
+        || !revisionerData
+    ) {
+        return;
+    }
+
+    const ender = parseInt(baseSplit[0]);
+    const launcher = parseInt(baseSplit[1]);
+    const revisioner = parseInt(baseSplit[2]);
+    const developmenter = parseInt(split[1]);
+
+    if (
+        ender >= 0
+        && launcher >= 0
+        && revisioner >= 0
+        && developmenter >= 0
+    ) {
+        const segments: AlphaSegments = {
+            ender,
+            launcher,
+            revisioner,
+            developmenter,
+        };
+
+        return segments;
+    }
+
+    return;
+}
+
+
 const isAlphaVersion = (
     value: string,
 ): boolean => {
-    return false;
+    const segments = splitAlphaSegments(value);
+
+    if (!segments) {
+        return false;
+    }
+
+    return true;
 }
 
 
 const getAlphaSegments = (
     value: string,
 ): AlphaSegments => {
-    return {
-        ender: 0,
-        launcher: 0,
-        revisioner: 0,
-        developmenter: 0,
-    };
+    const segments = splitAlphaSegments(value);
+
+    if (!segments) {
+        return {
+            ender: 0,
+            launcher: 0,
+            revisioner: 0,
+            developmenter: 0,
+        };
+    }
+
+    return segments;
+}
+
+
+const updateAlphaSegment = (
+    segments: AlphaSegments,
+    kind: AlphaSegmentKind,
+    type: AlphaUpdateType,
+) => {
+    const selector = segmentKindMap[kind];
+
+    if (!selector) {
+        return segments;
+    }
+
+    if (type === 'increase') {
+        segments[selector] += 1;
+    } else {
+        if (segments[selector] - 1 < 0) {
+            segments[selector] = 0;
+        } else {
+            segments[selector] -= 1;
+        }
+    }
+
+    return segments;
 }
 
 
@@ -35,7 +125,24 @@ const updateAlphaVersion = (
     kind: AlphaSegmentKind = 'development',
     type: AlphaUpdateType = 'increase',
 ): string => {
-    return value;
+    const segments = getAlphaSegments(value);
+
+    const updatedSegments = updateAlphaSegment(
+        segments,
+        kind,
+        type,
+    );
+
+    const {
+        ender,
+        launcher,
+        revisioner,
+        developmenter,
+    } = updatedSegments;
+
+    const alphaValue = `${ender}.${launcher}.${revisioner}-${developmenter}`;
+
+    return alphaValue;
 }
 // #endregion module
 
